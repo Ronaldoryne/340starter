990d1560ef51651d1aefae44dc07b8c84003366d
Ronaldoryne <rryne2001@gmail.com>
2025-08-01 13:41:20 +0200

    add

--- server.js
+++ server.js
@@ -2,44 +2,68 @@
  * This server.js file is the primary file of the 
  * application. It is used to control the project.
  *******************************************/
+
 /* ***********************
  * Require Statements
  *************************/
 const inventoryRoute = require("./routes/inventoryRoute");
-const baseController = require("./controllers/baseController")
-const express = require("express")
-const expressLayouts = require("express-ejs-layouts")
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
+const baseController = require("./controllers/baseController");
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const env = require("dotenv").config();
+const app = express();
+const static = require("./routes/static");
+const utilities = require("./utilities/");
 
 /* ***********************
  * View Engine and Templates
  *************************/
-app.set("view engine", "ejs")
-app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at views root
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set("layout", "./layouts/layout"); // not at views root
 
 /* ***********************
  * Routes
  *************************/
-app.use(static)
+app.use(static);
 
-// Index route
-app.get("/", baseController.buildHome);
+// ✅ HOME ROUTE FIRST
+app.get("/", utilities.handleErrors(baseController.buildHome))
 
-// Inventory routes
-app.use("/inv", inventoryRoute)
+// ✅ INVENTORY ROUTES NEXT
+app.use("/inv", inventoryRoute);
+
+// ✅ 404 ROUTE MUST BE LAST
+app.use(async (req, res, next) => {
+  next({ status: 404, message: "Sorry, we appear to have lost that page." });
+});
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
+
+
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT || 3000
-const host = process.env.HOST || '0.0.0.0'
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || "0.0.0.0";
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, host, () => {
-  console.log(`app listening on ${host}:${port}`)
-})
+  console.log(`app listening on ${host}:${port}`);
+});
